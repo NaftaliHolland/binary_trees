1dca13e19dd23564f6b0280488e8cc097f4724ef
Naftali Holland <naftaliholland01@gmail.com>
2023-12-01 00:51:04 +0300

    Removed a trailing white space

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -23,6 +23,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	size_left = binary_tree_size(tree->left);
 	size_right = binary_tree_size(tree->right);
-	
+
 	return ((size_left + size_right) + 1);
 }
