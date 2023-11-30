516ab547fbe503dfea7397358b580677bd2e48f2
Naftali Holland <naftaliholland01@gmail.com>
2023-12-01 01:21:28 +0300

    A function that counts the number of leaves in a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_leaves - Counts the number of leaves
+ *
+ * @tree: Pointer to the root node
+ *
+ * Return: Number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	binary_tree_leaves(tree->left);
+	binary_tree_leaves(tree->right);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
